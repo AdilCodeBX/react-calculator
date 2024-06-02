66918a61a224e2dba61aa39a50d52130d74d20db
AdilCodeBX <jihadmdna@gmail.com>
2024-06-02 23:05:16 +0200

    first commit

---
+++ src/components/Display.jsx
@@ -0,0 +1,14 @@
+
+import PropTypes from 'prop-types';
+
+import './Display.css';
+
+const Display = ({ value }) => {
+  return <div className='display'>{value}</div>
+}
+
+Display.propTypes = {
+    value: PropTypes.string.isRequired
+}
+
+export default Display;
